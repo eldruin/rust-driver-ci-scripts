0109f466500bdbf34e5c153f9f7331f52c2fede6
Diego Barrios Romero <eldruin@gmail.com>
2020-02-10 19:14:21 +0100

    Install latest tagged tarpaulin release

--- coverage_before_script.sh
+++ coverage_before_script.sh
@@ -1,6 +1,7 @@
 set -euxo pipefail
 
-cargo install cargo-tarpaulin    || echo "cargo-tarpaulin already installed"
-cargo install cargo-update   || echo "cargo-update already installed"
-cargo install cargo-travis   || echo "cargo-travis already installed"
-cargo install-update -a
+bash <(curl https://raw.githubusercontent.com/xd009642/tarpaulin/master/travis-install.sh)
+#cargo install cargo-tarpaulin    || echo "cargo-tarpaulin already installed"
+#cargo install cargo-update   || echo "cargo-update already installed"
+#cargo install cargo-travis   || echo "cargo-travis already installed"
+#cargo install-update -a
