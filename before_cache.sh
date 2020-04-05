5bafd7738423977dfb6f8c4889003395bb8abbf1
Diego Barrios Romero <eldruin@gmail.com>
2020-04-05 12:02:51 +0200

    Improve caching

--- before_cache.sh
+++ before_cache.sh
@@ -2,4 +2,4 @@ set -euxo pipefail
 
 # Travis can't cache files that are not readable by "others"
 chmod -R a+r $HOME/.cargo
-rm -rf /home/travis/.cargo/registry
+rm -rf "$TRAVIS_HOME/.cargo/registry/src"
