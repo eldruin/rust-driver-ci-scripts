590dfa4094c03d60d6cf9f62e37c37c0be5dc813
Diego Barrios Romero <eldruin@gmail.com>
2022-08-08 09:11:23 +0200

    Improve no-std dependency replace matching

--- patch-no-std.sh
+++ patch-no-std.sh
@@ -1,3 +1,3 @@
-sed -i "s/linux-embedded-hal/#linux-embedded-hal/g" Cargo.toml
-sed -i "s/embedded-hal-mock/#embedded-hal-mock/g" Cargo.toml
-sed -i "s/criterion/#criterion/g" Cargo.toml
+sed -i "s/^linux-embedded-hal/#linux-embedded-hal/g" Cargo.toml
+sed -i "s/^embedded-hal-mock/#embedded-hal-mock/g" Cargo.toml
+sed -i "s/^criterion/#criterion/g" Cargo.toml
